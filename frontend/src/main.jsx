50f8beba008ad4912ea90d23c2d38a513009793d
amit-singh-mobcoder <amit.s@mobcoder.com>
2024-10-04 17:20:24 +0530

    user context api created

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { RoleProvider } from './context/Role.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RoleProvider } from "./context/Role.jsx";
+import { UserProvider } from "./context/User.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RoleProvider>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </RoleProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
